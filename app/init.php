761d7f4821ebd821de80a1f6bd298191ef1059ac
usepsh <58686569+usepsh@users.noreply.github.com>
2021-09-30 20:51:48 +0700

    Mengelola Assets

--- app/init.php
+++ app/init.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
 
 require_once 'core/App.php';
 require_once 'core/Controller.php';
+
+require_once 'core/Constants.php';
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,6 @@
-<h1>About Me</h1>
-<p>Halo, Nama Saya <?= $data['nama']; ?>, Umur Saya <?= $data['umur']; ?> Tahun. Saya adalah seorang <?= $data['pekerjaan']; ?></p>
+<div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL ?>/img/contoh.jpg" alt="wkwk" width="200" class="rounded-circle shadow">
+  <p>Halo, Nama Saya <?= $data['nama']; ?>, Umur Saya <?= $data['umur']; ?> Tahun. Saya adalah seorang <?= $data['pekerjaan']; ?></p>
+
+</div>
