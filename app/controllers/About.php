a979ae71d35b71a36d5684ee4d515bcbd653c9e3
usepsh <58686569+usepsh@users.noreply.github.com>
2021-09-30 09:49:47 +0700

    Membuat view dalam MVC

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,15 +1,24 @@
 <?php
 
-class About
+class About extends Controller
 {
 
-  public function index($nama = 'Usep', $pekerjaan = 'Mahasiswa')
+  public function index($nama = 'Usep', $pekerjaan = 'Mahasiswa', $umur = 20)
   {
-    echo "Halo, nama saya $nama, saya adalah $pekerjaan";
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+    $data['judul'] = 'About Me';
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/footer');
   }
 
   public function page()
   {
-    echo 'About/page';
+    $data['judul'] = 'Pages';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,2 @@
+<h1>About Me</h1>
+<p>Halo, Nama Saya <?= $data['nama']; ?>, Umur Saya <?= $data['umur']; ?> Tahun. Saya adalah seorang <?= $data['pekerjaan']; ?></p>
